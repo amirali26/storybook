34bcd0464f01991f0ee86e0fc9df73ca10259408
Amir Shojae <amirshojae@alphafx.co.uk>
2021-04-03 14:49:53 +0100

    testing

---
+++ dist/Button.js
@@ -0,0 +1,6 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+import { Button } from '@material-ui/core';
+var WWWButton = function () {
+    return _jsx(Button, { children: "hello world" }, void 0);
+};
+export default WWWButton;
