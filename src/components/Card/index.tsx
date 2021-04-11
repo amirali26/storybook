3b02351f1d80b538eb4b9ee48a7e3e059366c933
Amir Shojae <amirshojae@alphafx.co.uk>
2021-04-11 18:43:37 +0100

    custom styling on card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
+import clsx from 'clsx';
+
 import useStyles from './index.styles';
 
 export interface ContentProps {
   children: JSX.Element | JSX.Element[];
+  className?: string;
 }
 
 const Card: React.FC<ContentProps> = (props: ContentProps) => {
   const styles = useStyles();
-  return <div className={styles.root}>{props.children}</div>;
+  return (
+    <div className={clsx(styles.root, props.className)}>{props.children}</div>
+  );
 };
 
 export interface ContentProps {
   children: JSX.Element | JSX.Element[];
+  className?: string;
 }
 const Left: React.FC<ContentProps> = (props: ContentProps) => {
   const styles = useStyles();
-  return <div className={styles.left}>{props.children}</div>;
+  return (
+    <div className={clsx(styles.left, props.className)}>{props.children}</div>
+  );
 };
 
 const Right: React.FC<ContentProps> = (props: ContentProps) => {
   const styles = useStyles();
-  return <div className={styles.right}>{props.children}</div>;
+  return (
+    <div className={clsx(styles.right, props.className)}>{props.children}</div>
+  );
 };
 
 export const WWWCard = {
