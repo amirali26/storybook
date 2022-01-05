59ca4e1774f4f78482294a85d70a4888517cc1e5
amirali26 <amiralishojaee123@gmail.com>
2022-01-05 21:35:40 +0000

    removed chip input

--- src/components/ChipInput/index.tsx
+++
@@ -1,6 +0,0 @@
-import ChipInput, { Props } from "material-ui-chip-input";
-import React from 'react'
-
-const MultiTextInput: React.FC<Props> = (props) => <ChipInput {...props} />;
-
-export default MultiTextInput;
